7d8e5e927ce1d23b5b024cae0df4ad4af1fea9bd
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-11-20 14:14:22 +0100

    Client Done!

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,10 @@
 package ch.heigvd.api.calc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,6 +14,7 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
+    private final static int PORT = 12345;
 
     /**
      * Main function to run client
@@ -34,6 +38,54 @@ public class Client {
          */
 
         stdin = new BufferedReader(new InputStreamReader(System.in));
+        Socket server = null;
+        BufferedReader  in = null;
+        BufferedWriter out = null;
 
+        try {
+            /* Create socket to server + Make connection request on IP/port */
+            server = new Socket("192.168.1.122", PORT);
+
+            in  = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+
+            /* Read first message from server */
+            /* Must be WELCOME + available COMMANDS */
+            while (true) {
+                String line = in.readLine();
+                System.out.println(line);
+                if (line.equals("- ENDOFOPERATIONS CRLF"))
+                    break;
+            }
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Cannot create client I/O socket", ex);
+        }
+
+        while (true) {
+            try {
+                System.out.println("C: ");
+                String userInput = stdin.readLine();
+
+                out.write(userInput + "\r\n");
+                out.flush();    // /!\ ABSOLUTELY NEEDED /!\
+
+                String serverResponse = in.readLine();
+                if (serverResponse == null) {
+                    System.out.println("Server closed connection");
+                    break;
+                }
+
+                String[] tokens = serverResponse.split(" ");
+                System.out.println("S: " + serverResponse);
+                if (tokens[0].equals("RESULT")) {
+                    int result = Integer.parseInt(tokens[1]);
+                    System.out.println("Result as int: " + result);
+                }
+                if (userInput.equals("QUIT"))
+                    break;
+            } catch (Exception ex) {
+                LOG.log(Level.SEVERE, "Cannot create client I/O socket", ex);
+            }
+        }
     }
 }
