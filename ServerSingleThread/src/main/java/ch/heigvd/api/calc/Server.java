dc83c03dd02851d952e7ce04dee2380dd9b59c5f
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-11-20 14:54:09 +0100

    Server single threaded Done!

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -3,6 +3,7 @@ package ch.heigvd.api.calc;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +13,8 @@ import java.util.logging.Logger;
 public class Server {
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
+    private final static int PORT = 12345;
+    private final static String ENDOFCMD = "CRLF\r\n";
 
     /**
      * Main function to start the server
@@ -31,6 +34,23 @@ public class Server {
          *  The receptionist just creates a server socket and accepts new client connections.
          *  For a new client connection, the actual work is done by the handleClient method below.
          */
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(PORT);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, "Could not listen on port " + PORT, e);
+            System.exit(-1);
+        }
+
+        while (true) {
+            try {
+                Socket client = server.accept();
+                handleClient(client);
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, "Accept failed on port " + PORT, e);
+                System.exit(-1);
+            }
+        }
 
     }
 
@@ -50,6 +70,85 @@ public class Server {
          *     - Handle the message
          *     - Send to result to the client
          */
+        BufferedReader  in = null;
+        BufferedWriter out = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
 
+            out.write("WELCOME " + ENDOFCMD);
+            out.write("- AVAILABLE OPERATIONS " + ENDOFCMD);
+            out.write("- ADD 2 " + ENDOFCMD);
+            out.write("- MULT 2 " + ENDOFCMD);
+            out.write("- DIV 2 " + ENDOFCMD);
+            out.write("- ENDOFOPERATIONS " + ENDOFCMD);
+            out.flush();    // /!\ ABSOLUTELY NEEDED /!\
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            String res = "";
+            try {
+                String line = in.readLine();
+                if(line == null) {
+                    break;
+                }
+                String[] tokens = line.split(" ");
+                if(tokens.length == 0) {
+                    continue;
+                }
+                String cmd = tokens[0];
+                if(cmd.equals("ADD")) {
+                    if(tokens.length != 3) {
+                        out.write("ERROR " + ENDOFCMD);
+                        out.flush();
+                        continue;
+                    }
+                    int a = Integer.parseInt(tokens[1]);
+                    int b = Integer.parseInt(tokens[2]);
+                    res = "RESULT " + (a + b) + " " + ENDOFCMD;
+                    System.out.print(res);
+                    out.write(res);
+                    out.flush();
+                } else if(cmd.equals("MULT")) {
+                    if(tokens.length != 3) {
+                        out.write("ERROR " + ENDOFCMD);
+                        out.flush();
+                        continue;
+                    }
+                    int a = Integer.parseInt(tokens[1]);
+                    int b = Integer.parseInt(tokens[2]);
+                    res = "RESULT " + (a * b) + " " + ENDOFCMD;
+                    System.out.print(res);
+                    out.write(res);
+                    out.flush();
+                } else if(cmd.equals("DIV")) {
+                    if(tokens.length != 3) {
+                        out.write("ERROR " + ENDOFCMD);
+                        out.flush();
+                        continue;
+                    }
+                    int a = Integer.parseInt(tokens[1]);
+                    int b = Integer.parseInt(tokens[2]);
+                    res = "RESULT " + (a / b) + " " + ENDOFCMD;
+                    System.out.print(res);
+                    out.write(res);
+                    out.flush();
+                } else if(cmd.equals("QUIT")) {
+                    System.out.println("Closing resources and disconnecting client");
+                    System.out.println("END OF THREAD");
+                    out.close();
+                    in.close();
+                    clientSocket.close();
+                    break;
+                } else {
+                    out.write("ERROR 400 SYNTAX ERROR " + ENDOFCMD);
+                    out.flush();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
